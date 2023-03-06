cbcef6f734a1b595bffcf86fcc258779a4a0c68b
EnTeVi <andriy.lytvynchuk@gmail.com>
2023-03-06 20:42:48 +0200

    r

--- app.js
+++ app.js
@@ -1,113 +1,199 @@
-const fs = require('fs/promises');
-const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 
-// ************** Create folder
+app.get('/welcome', (req, res) => {
+    res.send('dsfsdgds')
+    res.end();
+})
 
-const createFolder = (folder) => {
-    fs.mkdir(`${folder}`, (err) => {
-        console.log(err);
-    })
-}
-
-// createFolder('male');
-// createFolder('female');
-
-// *************** Create file
-
-const createFile = async (folderName, fileName, gender, name, age) => {
-    fs.appendFile(`./${folderName}/${fileName}.json`, JSON.stringify(
-        {
-            "gender": gender,
-            "name": name,
-            "age": age
-        }
-    ), (err) => {
-        console.log(err);
-    })
-}
-
-// createFile('male', 'Sergiy', 'male', 'Sergiy', 23);
-// createFile('male', 'Andriy', 'male', 'Andriy', 24);
-// createFile('male', 'Alina', 'female', 'Alina', 25);
-// createFile('male', 'Viktoria', 'female', 'Viktoria', 26);
-// createFile('female', 'Natalia', 'female', 'Natalia', 25);
-// createFile('female', 'Ira', 'female', 'Ira', 24);
-// createFile('female', 'Stepan', 'male', 'Stepan', 23);
-// createFile('female', 'Viktor', 'male', 'Viktor', 22);
-
-// ************** Sorting file
-
-const sorting = async (readFolder, writeFolder, gender) => {
-    const folderPath = path.join(__dirname, readFolder);
-
-    const files = await fs.readdir(folderPath);
-
-    for (const file of files) {
-        const filePath = path.join(folderPath, file);
-        const data = await fs.readFile(filePath);
-        const user = JSON.parse(data);
-
-        if (user.gender === gender) {
-            await fs.rename(filePath, path.join(__dirname, writeFolder, file));
-        }
+const users = [
+    {
+        name: 'Oleh',
+        age: 18,
+        gender: 'male'
+    },
+    {
+        name: 'Dima',
+        age: 19,
+        gender: 'male'
+    },
+    {
+        name: 'Alisa',
+        age: 20,
+        gender: 'female'
+    },
+    {
+        name: 'Victor',
+        age: 22,
+        gender: 'female'
+    },
+    {
+        name: 'Cocos',
+        age: 82,
+        gender: 'mixed'
     }
-}
+]
 
+app.get('/users', (req, res) => {
+    res.json(users);
+})
 
-// sorting('male', 'female', 'female');
-// sorting('female', 'male', 'male');
+app.get('/users/:userId', (req, res) => {
+    const {userId} = req.params;
+    const user = users[+userId];
+    res.json(user);
+})
 
-
-// **********************************************************
-// hw2
-
-// const fs = require('node:fs');
-
-
-const createFolder = (folder) => {
-    fs.mkdir(`${folder}`, (err) => {
-        console.log(err);
+app.post('/users', (req, res) => {
+    const body = req.body;
+    users.push(body);
+    res.json({
+        message: 'User created!!!'
     })
-}
+})
 
-const createFolderLoop = (mainFolder, folder) => {
-    for (let i = 0; i<5; i++) {
-        fs.mkdir(`./${mainFolder}/${folder}-${i}`, (err) => {
-            console.log(err);
-        })
-    }
-}
-
-const createFileLoop = (mainFolder, fileName) => {
-    for (let i = 0; i<5; i++) {
-        fs.appendFile(`./${mainFolder}/${fileName}-${i}.txt`, `This is ${i} file`,(err) => {
-            console.log(err);
-        })
-    }
-}
-
-
-const readType = () => {
-    fs.readdir('./hw2', (err, data) => {
-        console.log(data);
-
-        for (const fileName of data) {
-            fs.stat(`./hw2/${fileName}`, (err1, stats) => {
-                console.log(stats.isDirectory());
-
-                if (stats.isFile()) {
-                    fs.readFile(`./hw2/${fileName}`, (err2, data) => {
-                        console.log(data.toString());
-                    })
-                }
-            })
-        }
+app.put('/users', (req, res) => {
+    const {userId} = req.params;
+    const updateUser = req.body;
+    users[+userId] = updateUser;
+    res.status(300).json({
+        message: 'kurva math update',
+        data: users[+userId]
     })
-}
+})
+
+app.delete('/users/:userId', (req, res) => {
+    const {userId} = req.params;
+    users.splice(+userId, 1);
+    res.status(200).json({
+        message: 'User delete bich'
+    })
+})
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server to started if you give me ${PORT} dollars, else I kill you`);
+});
 
 
-// createFolder('hw2');
-// createFolderLoop('hw2', 'folder');
-// createFileLoop('hw2', 'file');
-// readType();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const event = require('node:events');
+//
+// const eventEmitter = new event();
+//
+// eventEmitter.on('click', (data) => {
+//     console.log('Click on me, mutherfucker');
+//     console.log(data);
+// }); /*можна викликати багато раз*/
+
+// eventEmitter.emit('click', {name: 'Patron'});
+
+// eventEmitter.once('clickOne', (data) => {
+//     console.log(data);
+//     console.log('This is emmit enter one return');
+// }) /*можна викликати один раз*/
+//
+// eventEmitter.emit('clickOne', {name: 'Niger'});
+
+
+// streem
+// типи стрімів read, write, duplex, transform
+
+// const fs = require('fs');
+// const path = require("path");
+//
+// const readStream = fs.createReadStream(path.join('test', 'text.txt'));
+// // прийом нового шляху для чанків і перезаписування
+// const writeStream = fs.createWriteStream(path.join('test', 'text3.txt'))
+//
+// // записування
+// // readStream.on('data', (chunk) => {
+// //     writeStream.write(chunk);
+// // })
+//
+// const handleError = () => {
+//     console.log('Error!');
+//     // знищення readstream
+//     readStream.destroy();
+//     writeStream.end('Error white reading file');
+// }
+//
+// // простіше записування
+// readStream
+//     .on('error m', handleError)
+//     .pipe(writeStream);
